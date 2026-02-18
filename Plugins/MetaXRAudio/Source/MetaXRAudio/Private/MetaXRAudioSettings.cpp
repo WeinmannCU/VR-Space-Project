742b43fb2c37644f6b178dd10a18dbdfbe2f4ed3
CarolineYoder <carolineyoder@cedarville.edu>
2026-02-18 15:36:32 -0500

    remove plugins

--- Plugins/MetaXRAudio/Source/MetaXRAudio/Private/MetaXRAudioSettings.cpp
+++
@@ -1,6 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MetaXRAudioSettings.h"
-
-UMetaXRAudioSettings::UMetaXRAudioSettings() : VoiceLimit(64) {}
